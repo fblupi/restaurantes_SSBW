6f96c29994c1fd71120d7a741fda5cbc2e3f1135
jose999 <jmmoyabaena@correo.ugr.es>
2017-07-06 09:08:02 +0200

    remove images

--- restaurantes/views.py
+++ restaurantes/views.py
@@ -90,8 +90,8 @@ def aniade(request):
             r.borough = self.cleaned_data['borough']
             r.address = a
 
-            if len(request.FILES) != 0:
-                r.photo.put(request.FILES['photo'], content_type = request.FILES['photo'].content_type)
+            #if len(request.FILES) != 0:
+            #    r.photo.put(request.FILES['photo'], content_type = request.FILES['photo'].content_type)
 
             r.save()
 
