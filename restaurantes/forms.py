d64b32b56e6e6160795aeed00b7aa7c8cc949846
jose999 <jmmoyabaena@correo.ugr.es>
2017-07-06 08:58:23 +0200

    remove func save

--- restaurantes/forms.py
+++ restaurantes/forms.py
@@ -10,34 +10,3 @@ class RestaurantForm(forms.Form):
     cuisine = forms.CharField(required=True, label='Tipo de cocina')
     borough = forms.CharField(required=True, label='Barrio')
     photo   = forms.FileField(required=False, label='Foto')
-
-    def save(self, commit=True):
-        req_url = 'http://maps.googleapis.com/maps/api/geocode/xml?address=' + self.cleaned_data['name'] + ' ' + self.cleaned_data['city']
-
-        tree = etree.parse(req_url)
-
-        addressXML = tree.xpath('//address_component')
-        locationXML = tree.xpath('//location')
-
-        buildingXML = addressXML[0].xpath('//long_name/text()')[0]
-        streetXML = addressXML[1].xpath('//long_name/text()')[1]
-        cityXML = addressXML[2].xpath('//long_name/text()')[2]
-        zipcodeXML = int(addressXML[6].xpath('//long_name/text()')[6])
-        coordXML = [float(locationXML[0].xpath('//lat/text()')[0]), float(locationXML[0].xpath('//lng/text()')[0])]
-
-        a = addr(building=buildingXML, street=streetXML, city=cityXML, zipcode=zipcodeXML, coord=coordXML)
-
-        r = restaurants()
-
-        r.name = self.cleaned_data['name']
-        r.restaurant_id = restaurants.objects.count() + 1
-        r.cuisine = self.cleaned_data['cuisine']
-        r.borough = self.cleaned_data['borough']
-        r.address = a
-
-        if len(request.FILES) != 0:
-            r.photo.put(request.FILES['photo'], content_type = request.FILES['photo'].content_type)
-
-        r.save()
-
-        return r
